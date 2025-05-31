7554f52bc0c4d397ab64dd81d64164ab5092bf2b
farshadmrd <fa.moradi2000@gmail.com>
2025-06-01 01:33:43 +0200

    fix the bug of get request

--- service/views.py
+++ service/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from .models import Service, Type
 from .serializers import ServiceSerializer, TypeSerializer
 

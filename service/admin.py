faeebc56ae2275321f02d39c50d4badafbe717c5
farshadmrd <fa.moradi2000@gmail.com>
2025-06-01 01:05:04 +0200

    add the admin page of service

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Service, Type
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active')
+    search_fields = ('title', 'description')
+    list_filter = ('is_active',)
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'service', 'price', 'is_active')
+    search_fields = ('name', 'description')
+    list_filter = ('is_active', 'service')

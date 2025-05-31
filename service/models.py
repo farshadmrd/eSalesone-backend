524f4782432e4f612198cee370ddb1b95a202848
farshadmrd <fa.moradi2000@gmail.com>
2025-06-01 01:03:04 +0200

    add is_active field

--- service/models.py
+++ service/models.py
@@ -6,7 +6,8 @@ class Service(models.Model):
     """
     title = models.CharField(max_length=255)
     logo = models.ImageField(upload_to='media/logos/')
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
@@ -17,8 +18,9 @@ class Type(models.Model):
     """
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name

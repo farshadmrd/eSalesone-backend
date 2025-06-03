a48ef20ac0f2c698e361c2311b20976544c18133
farshadmrd <fa.moradi2000@gmail.com>
2025-06-03 20:49:28 +0200

    Add recommended field

--- service/models.py
+++ service/models.py
@@ -1,13 +1,14 @@
 import uuid
 from django.db import models
+from core.validators import validate_logo_file_extension
 
 class Service(models.Model):
     """
     Represents offered services.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255, unique=True)
-    logo = models.ImageField(upload_to='media/logos/')
+    title = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='media/logos/', validators=[validate_logo_file_extension])
     description = models.TextField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
@@ -24,6 +25,7 @@ class Type(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
+    recommended = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
